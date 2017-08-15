d543d609c6d6c4dd24746a42a71dd5e231b95793
Brian Hart <bbhart@bbhart.com>
2017-08-14 21:53:52 -0700

    The picker finally works. I was dumb.

--- SwiftTemperatureConverter/ViewController.swift
+++ SwiftTemperatureConverter/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    // Min and max to seed the picker with
+    let minTemp = -40
+    let maxTemp = 40
+    var pickerData = [String]()
+    
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        pickerData = Array(minTemp...maxTemp).map( { i in "\(i)°C"} )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // returns the number of 'columns' to display.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        print("Call to titleforrow for row \(row)")
+        return pickerData[row]
+    }
 
 
 }
+
 
